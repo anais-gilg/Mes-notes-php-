c69c911de4178e99da60563598d4c7ef12435f8b
anais-gilg <114387786+anais-gilg@users.noreply.github.com>
2022-11-20 14:51:41 +0100

    Les différents types opérateurs

---
+++ syntaxe/opérateur/arithmétique.php
@@ -0,0 +1,17 @@
+<?php 
+$a = 10;
+$b = 20;
+
+echo -$a;
+echo $a + $b . "<br>";
+echo $a - $b . "<br>";
+echo $a * $b . "<br>";
+echo $b / $a . "<br>";
+echo $b % $a . "<br>";
+echo $b ** $a . "<br>";
+echo $a++ . "<br>";
+echo $a-- . "<br>";
+
+
+
+?>
